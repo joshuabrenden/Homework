1dbf6f6e67cd080fcea70f6043f90ffa06c26bb2
Josh Brenden <joshuabrenden@gmail.com>
2016-03-10 22:18:46 -0700

    Completed quicksort

--- Pgm1/src/QuickSort.java
+++ Pgm1/src/QuickSort.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
 import java.util.List;
 
 public class QuickSort implements ISort {
 
 	List<Person> unsortedPersons;
 	Person[] personArray;
+	private List<Person> sortedPersons;
 
 	public QuickSort(List<Person> unsortedPersons) {
 		this.unsortedPersons = unsortedPersons;
@@ -13,46 +15,56 @@ public class QuickSort implements ISort {
 	public void execute() {
 		personArray = unsortedPersons.toArray(new Person[unsortedPersons.size()]);
 
-		Integer high = personArray.length - 1;
-		Integer low = 0;
+		int high = personArray.length - 1;
+		int low = 0;
 
 		quickSort(low, high);
+		sortedPersons = Arrays.asList(personArray);
 	}
 
 	@Override
 	public List<Person> getSortedPersons() {
-		return unsortedPersons;
+		return sortedPersons;
 
 	}
 
-	private void quickSort(Integer low, Integer high){
-		Integer lowIndex = low;
-		Integer highIndex = high;
-		
-		Person pivotPerson = personArray[lowIndex + (highIndex - lowIndex) / 2];
-		if(highIndex - lowIndex >= 1){
-			while(lowIndex <= highIndex){
-				
-				while(personArray[lowIndex].compareTo(pivotPerson) < 0){
-					lowIndex++;
-				}
-				
-				while(personArray[highIndex].compareTo(pivotPerson) > 0){
-					highIndex--;
-				}
-				
-				if(lowIndex <= highIndex){
-					rotate(lowIndex, highIndex);
-					lowIndex++;
-					highIndex++;
-				}
+	private void quickSort(int low, int high) {
+		int lowIndex = low;
+		int highIndex = high;
+		int pivotIndex = low + (high - low) / 2;
+
+		Person pivotPerson = personArray[pivotIndex];
+
+		while (lowIndex <= highIndex) {
+
+			while (personArray[lowIndex].compareTo(pivotPerson) < 0) {
+				lowIndex++;
 			}
-			
-			
+
+			while (personArray[highIndex].compareTo(pivotPerson) > 0) {
+				highIndex--;
+			}
+
+			if (lowIndex <= highIndex) {
+				rotate(lowIndex, highIndex);
+				lowIndex++;
+				highIndex--;
+			}
 		}
+
+		if (low < highIndex) {
+			quickSort(low, highIndex);
+		}
+
+		if (lowIndex < high) {
+			quickSort(lowIndex, high);
+		}
+
 	}
-	
-	private void rotate(Integer low, Integer high){
-	
+
+	private void rotate(int low, int high) {
+		Person tempPerson = personArray[low];
+		personArray[low] = personArray[high];
+		personArray[high] = tempPerson;
 	}
 }
