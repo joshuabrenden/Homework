5c382a1cc34a11b2a66d220642f35a963ff4ec70
Josh Brenden <joshuabrenden@gmail.com>
2016-03-10 23:37:28 -0700

    Completed Insertion Sort and larger test files.

--- Pgm1/src/InsertionSort.java
+++ Pgm1/src/InsertionSort.java
@@ -1,22 +1,42 @@
+import java.util.Arrays;
 import java.util.List;
 
 public class InsertionSort implements ISort {
-	
+
 	List<Person> unsortedPersons;
 	List<Person> sortedPersons;
-	
-	public InsertionSort(List<Person> unsortedPersons){
+	Person[] personArray;
+
+	public InsertionSort(List<Person> unsortedPersons) {
 		this.unsortedPersons = unsortedPersons;
 	}
 
 	@Override
 	public void execute() {
-		sortedPersons = unsortedPersons;
+		personArray = unsortedPersons.toArray(new Person[unsortedPersons.size()]);
+		insertionSort();
+
+		sortedPersons = Arrays.asList(personArray);
 	}
 
 	@Override
 	public List<Person> getSortedPersons() {
 		return sortedPersons;
-		
+
+	}
+
+	private void insertionSort() {
+		Person tempPerson;
+		for (int i = 1; i < personArray.length; i++) {
+			for (int j = i; j > 0; j--) {
+
+				if (personArray[j].compareTo(personArray[j - 1]) < 0) {
+
+					tempPerson = personArray[j];
+					personArray[j] = personArray[j - 1];
+					personArray[j - 1] = tempPerson;
+				}
+			}
+		}
 	}
 }
--- Pgm1/src/Pgm1.java
+++ Pgm1/src/Pgm1.java
@@ -1,30 +1,53 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Pgm1 {
 	public static void main(String[] args) {
 
-		String inputFileName = "Input.txt";
-		ReadFile readFile = new ReadFile(inputFileName);
-		
-		PersonBuilder builder = new PersonBuilder();
-		List<Person> persons = builder.buildPersonList(readFile.buildArrayFromFile());
-		Integer lineCount = readFile.getLineCount();
-		
-		//Change to a sort factory
+		for (String inputFileName : loadTestFiles()) {
+			ReadFile readFile = new ReadFile(inputFileName);
+
+			PersonBuilder builder = new PersonBuilder();
+			List<Person> persons = builder.buildPersonList(readFile.buildArrayFromFile());
+			Integer lineCount = readFile.getLineCount();
+
+			runQuickSort(persons, inputFileName, lineCount);
+			runInsertionSort(persons, inputFileName, lineCount);
+			runMergeSort(persons, inputFileName, lineCount);
+		}
+	}
+
+	public static List<String> loadTestFiles() {
+		List<String> testFiles = new ArrayList<>();
+		testFiles.add("10People.txt");
+		testFiles.add("100People.txt");
+		testFiles.add("1000People.txt");
+		return testFiles;
+	}
+	
+	public static void runQuickSort(List<Person> persons, String inputFileName, Integer lineCount){
 		QuickSort quickSort = new QuickSort(persons);
 		quickSort.execute();
-		OutFile quickSortResults = new OutFile("QuickSortResults.txt", quickSort.getSortedPersons(), lineCount);
+		OutFile quickSortResults = new OutFile("QuickSortResults-" + inputFileName, quickSort.getSortedPersons(),
+				lineCount);
 		quickSortResults.buildOutFile();
-		
+	}
+	
+	public static void runInsertionSort(List<Person> persons, String inputFileName, Integer lineCount){
 		InsertionSort insertionSort = new InsertionSort(persons);
 		insertionSort.execute();
-		OutFile insertionSortResults = new OutFile("InsertionSortResults.txt", insertionSort.getSortedPersons(), lineCount);
+		OutFile insertionSortResults = new OutFile("InsertionSortResults-" + inputFileName,
+				insertionSort.getSortedPersons(), lineCount);
 		insertionSortResults.buildOutFile();
-
+	}
+	
+	public static void runMergeSort(List<Person> persons, String inputFileName, Integer lineCount){
 		MergeSort mergeSort = new MergeSort(persons);
 		mergeSort.execute();
-		OutFile mergeSortResults = new OutFile("MergeSortResults.txt", mergeSort.getSortedPersons(), lineCount);
+		OutFile mergeSortResults = new OutFile("MergeSortResults-" + inputFileName, mergeSort.getSortedPersons(),
+				lineCount);
 		mergeSortResults.buildOutFile();
 	}
+	
 
 }
